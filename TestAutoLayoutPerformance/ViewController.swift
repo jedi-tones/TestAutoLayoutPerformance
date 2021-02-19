32539aeed5c0abd962c7cefe6d4e3b08e5f6e967
Denis Shchigolev <denis.shchigolev@yandex.ru>
2021-02-19 16:09:06 +0300

    Initial Commit

---
+++ TestAutoLayoutPerformance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAutoLayoutPerformance
+//
+//  Created by Денис Щиголев on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
